504f4f2f904fbc8b347ebf0d5ee1bf2c1a03f859
Vaibhav0x <rajjvaibhavv121@gmail.com>
2025-08-01 00:42:55 +0530

    env file used for the api url

--- src/services/api/apiClient.js
+++ src/services/api/apiClient.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import { API_BASE_URL } from '../../utils/constants';
 
+const API_URL = process.env.REACT_APP_DJANGO_API_BASE_URL;
+
+console.log(API_URL);
 export const apiClient = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
